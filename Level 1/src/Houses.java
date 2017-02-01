27cb6684d9c94fd1e0cae91dcac9f9eb90f304b1
DavidThorpe123 <league@WorthyMini-4.attlocal.net>
2017-01-31 17:16:49 -0800

    stuff

---
+++ Level 1/src/Houses.java
@@ -0,0 +1,10 @@
+import org.teachingextensions.logo.Tortoise;
+
+public class Houses {
+	public static void main(String[] args) {
+		Tortoise.penUp();
+		Tortoise.turn(230);
+		Tortoise.move(300);
+		Tortoise.turn(130);
+	}
+}
