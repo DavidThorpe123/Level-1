7f26168dd1f37f1af5c8efc4f4b1db7867de1f1b
Anthony Cerruti <aprime2003@gmail.com>
2017-02-14 18:20:26 -0800

    adfa

--- Level 1/src/SimpleCalculator.java
+++ Level 1/src/SimpleCalculator.java
@@ -1,4 +1,6 @@
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
 import javax.swing.JButton;
@@ -7,7 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class SimpleCalculator {
+public class SimpleCalculator implements ActionListener {
 	JFrame jframe;
 	JButton jbutton;
 	JButton sub;
@@ -49,7 +51,10 @@ public class SimpleCalculator {
 		multiply.setText("Multiply");
 		divide.setText("Divide");
 		answer.setText("55");
-
+		multiply.addActionListener(this);
+		sub.addActionListener(this);
+		divide.addActionListener(this);
+		jbutton.addActionListener(this);
 		jframe.pack();
 		jframe.setVisible(true);
 	}
@@ -59,4 +64,64 @@ public class SimpleCalculator {
 		Scanner s = new Scanner(System.in);
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == jbutton) {
+			System.out.println("Adding");
+			answer.setText(add());
+		}
+		if (e.getSource() == sub) {
+			System.out.println("Subcracting");
+			answer.setText(subb());
+
+		}
+		if (e.getSource() == multiply) {
+			System.out.println("Multiplying");
+			answer.setText(multiplyy());
+		}
+		if (e.getSource() == divide) {
+			System.out.println("Dividing");
+			answer.setText(dvide());
+		}
+	}
+
+	public String add() {
+		input.getText();
+		int num1 = Integer.parseInt(input.getText());
+		inputwo.getText();
+		int num2 = Integer.parseInt(inputwo.getText());
+
+		String answer2 = Integer.toString(num1 + num2);
+		return answer2;
+
+	}
+
+	public String multiplyy() {
+		input.getText();
+		int num1 = Integer.parseInt(input.getText());
+		inputwo.getText();
+		int num2 = Integer.parseInt(inputwo.getText());
+
+		String answerr = Integer.toString(num1 * num2);
+		return answerr;
+	}
+
+	public String dvide() {
+		input.getText();
+		float num1 = Float.parseFloat(input.getText());
+		inputwo.getText();
+		float num2 = Float.parseFloat(inputwo.getText());
+		String answerrr = Float.toString(num1 / num2);
+		return answerrr;
+	}
+
+	public String subb() {
+		input.getText();
+		int num1 = Integer.parseInt(input.getText());
+		inputwo.getText();
+		int num2 = Integer.parseInt(inputwo.getText());
+		String answerrrr = Integer.toString(num1 - num2);
+		return answerrrr;
+	}
 }
