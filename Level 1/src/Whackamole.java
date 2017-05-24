63a95f5c7717d6af489f863b5f5250fbc75fa7be
Ollie Pai <ollie.pai2003@gmail.com>
2017-05-23 18:00:10 -0700

    stuff

---
+++ Level 1/src/Whackamole.java
@@ -0,0 +1,15 @@
+import javax.swing.JButton;
+
+public class Whackamole {
+	JButton button;
+
+	public static void main(String[] args) {
+
+	}
+
+	void drawButtons() {
+		for (int i = 0; i < 30; i++) {
+			JButton button;
+		}
+	}
+}
