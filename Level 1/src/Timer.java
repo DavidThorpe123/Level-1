459993a82f83dbeae1d4e62d6cf5d781a84dc22d
BuildTools <unconfigured@null.spigotmc.org>
2017-06-24 16:07:30 -0700

    stuff

--- Level 1/src/Timer.java
+++ Level 1/src/Timer.java
@@ -4,28 +4,30 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class Timer implements ActionListener {
 	JFrame frame;
 	JPanel panel;
 	JButton button;
-	JTextField field;
+	JButton startbutton;
 
 	Timer() {
 		frame = new JFrame();
 		panel = new JPanel();
+		startbutton = new JButton();
 		button = new JButton();
-		field = new JTextField();
+
 		Dimension big = new Dimension(400, 400);
 		Dimension frameSize = new Dimension(750, 750);
+		panel.add(startbutton);
 		frame.add(panel);
 		panel.add(button);
-		panel.add(field);
-		field.setSize(big);
+
 		button.setSize(big);
-		field.setText(" ");
+		button.setText("Start timer");
+
 		frame.setVisible(true);
 		frame.setSize(frameSize);
 
@@ -33,20 +35,20 @@ public class Timer implements ActionListener {
 
 	public static void main(String[] args) {
 		Timer t = new Timer();
-	}
 
-	public int getTime() {
-		String time = field.getText();
-		int time2 = Integer.parseInt(time);
-		System.out.println(time2);
-		return time2;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+
 		// TODO Auto-generated method stub
 		if (arg0.getSource() == button) {
-			System.out.println(getTime());
+			String question = JOptionPane.showInputDialog(null, "Number of Seconds?");
+			int question2 = Integer.parseInt(question);
+			System.out.println(question2);
+		}
+		if (arg0.getSource() == startbutton) {
+
 		}
 	}
 
