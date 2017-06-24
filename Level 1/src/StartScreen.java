cbd3c08536c7ff7e675610951b24a8276ef57e12
Ganymede007 <moyhuro@gmail.com>
2017-06-24 14:59:39 -0700

    Stuff

--- Level 1/src/StartScreen.java
+++ Level 1/src/StartScreen.java
@@ -20,20 +20,18 @@ public class StartScreen implements ActionListener {
 		panel = new JPanel();
 		startbutton = new JButton();
 		startbuttontimer = new JButton();
-		area = new JTextArea();
+
 		GridLayout grid = new GridLayout(2, 0);
 		Dimension framesize = new Dimension(400, 400);
 		Dimension Big = new Dimension(200, 50);
-		Dimension areasize = new Dimension(150, 150);
 		startbuttontimer.setPreferredSize(Big);
 		startbuttontimer.setText("Start Timer");
 		startbutton.setPreferredSize(Big);
 		startbutton.setText("Start Calculator");
-		area.setText("David's Utilites");
-		area.setSize(areasize);
+
 		frame.add(panel);
 		frame.setLayout(grid);
-		panel.add(area);
+
 		panel.add(startbutton);
 		panel.add(startbuttontimer);
 		frame.setVisible(true);
--- Level 1/src/Timer.java
+++ Level 1/src/Timer.java
@@ -1,9 +1,13 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Timer {
+public class Timer implements ActionListener {
 	JFrame frame;
 	JPanel panel;
 	JButton button;
@@ -14,15 +18,36 @@ public class Timer {
 		panel = new JPanel();
 		button = new JButton();
 		field = new JTextField();
+		Dimension big = new Dimension(400, 400);
+		Dimension frameSize = new Dimension(750, 750);
 		frame.add(panel);
 		panel.add(button);
 		panel.add(field);
+		field.setSize(big);
+		button.setSize(big);
+		field.setText(" ");
 		frame.setVisible(true);
-		frame.pack();
+		frame.setSize(frameSize);
 
 	}
 
 	public static void main(String[] args) {
 		Timer t = new Timer();
 	}
+
+	public int getTime() {
+		String time = field.getText();
+		int time2 = Integer.parseInt(time);
+		System.out.println(time2);
+		return time2;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if (arg0.getSource() == button) {
+			System.out.println(getTime());
+		}
+	}
+
 }
