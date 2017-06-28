8b6927d2bcdfb119e8df711d5f9fe60b5bce5f49
BuildTools <unconfigured@null.spigotmc.org>
2017-06-27 20:08:46 -0700

    stuff

--- Level 1/src/Timer.java
+++ Level 1/src/Timer.java
@@ -6,38 +6,35 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Timer implements ActionListener {
 	JFrame frame;
 	JPanel panel;
 	JButton button;
 	JButton startbutton;
-	JTextArea area;
-
-	public String myString() {
-		String question = JOptionPane.showInputDialog(null, "Number of Seconds?");
-		int question2 = Integer.parseInt(question);
-		String question3 = Integer.toString(question2);
-		return question3;
-	}
+	JTextField field;
+	boolean hasSetNumber = false;
 
 	Timer() {
 		frame = new JFrame();
 		panel = new JPanel();
 		startbutton = new JButton();
 		button = new JButton();
-		area = new JTextArea();
+		field = new JTextField();
 
 		Dimension big = new Dimension(400, 400);
 		Dimension frameSize = new Dimension(750, 750);
 		panel.add(startbutton);
 		frame.add(panel);
 		panel.add(button);
-
+		field.setText("                    ");
+		panel.add(field);
 		button.setSize(big);
-		button.setText("Start timer");
-
+		button.setText("Click this button to set the time");
+		button.addActionListener(this);
+		startbutton.addActionListener(this);
+		startbutton.setText("Click this to actually start counting down");
 		frame.setVisible(true);
 		frame.setSize(frameSize);
 
@@ -53,9 +50,15 @@ public class Timer implements ActionListener {
 
 		// TODO Auto-generated method stub
 		if (arg0.getSource() == button) {
-			System.out.println(myString());
+			String question = JOptionPane.showInputDialog(frame, "Number of Seconds?");
+			int question2 = Integer.parseInt(question);
+			String question3 = Integer.toString(question2);
+			field.setText(question3);
+			hasSetNumber = true;
 		}
-		if (arg0.getSource() == startbutton) {
+		if (arg0.getSource() == startbutton && hasSetNumber == false) {
+			JOptionPane.showMessageDialog(frame, "Choose a number!");
+		} else if (arg0.getSource() == startbutton && hasSetNumber == true) {
 
 		}
 	}
